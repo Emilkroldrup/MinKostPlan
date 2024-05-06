a11e3852fc60c9b79256350bd5e2e21e21737351
Emilkr <EmilKroldrup@outlook.com>
2024-05-06 21:24:32 +0200

    refactor: :lipstick: Changed up the style of the page
    Changed the css completely around and made the js file easier to read, also updated the html file to be easier to read aswell

--- src/main/resources/static/javascript/slideShow.js
+++ src/main/resources/static/javascript/slideShow.js
@@ -1,17 +1,25 @@
-let slideIndex = 0;
-
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        let img = slides[i].getElementsByTagName('img')[0];
-        img.style.opacity = (i === slideIndex ? "1" : "0");
-    }
-    slideIndex = (slideIndex + 1) % slides.length;
-    setTimeout(showSlides, 6000);
-}
-
-
-// Initialize the slideshow
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(showSlides, 1000);
+    const slides = document.querySelectorAll('.mySlides');
+    let currentSlide = 0;
+
+    function showSlide() {
+        // Hide all slides by setting their opacity to 0
+        slides.forEach((slide) => {
+            slide.style.opacity = '0';
+            slide.classList.remove('active');
+        });
+
+        // Make the current slide visible
+        slides[currentSlide].classList.add('active');
+        slides[currentSlide].style.opacity = '1';
+
+        // Move to the next slide
+        currentSlide = (currentSlide + 1) % slides.length;
+
+        // Set a timeout to show the next slide after 3 seconds
+        setTimeout(showSlide, 4000);
+    }
+
+    // Initialize the slideshow
+    showSlide();
 });
