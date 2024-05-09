482f666139198e80c57d5f5166aa97450acf2934
Emilkr <Emilkroldrup@outlook.com>
2024-05-09 22:28:31 +0200

    feat: Refactor Ingredient class to include constructors, getters, and setters

--- src/main/java/minkostplan/application/entity/Ingredient.java
+++ src/main/java/minkostplan/application/entity/Ingredient.java
@@ -6,5 +6,45 @@ public class Ingredient {
     private String name;
     private String description;
 
-    // Constructors, Getters, and Setters
+    public Ingredient() {
+    }
+
+    public Ingredient(Long ingredientId, String name, String description) {
+        this.ingredientId = ingredientId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(Long ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "ingredientId=" + ingredientId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
