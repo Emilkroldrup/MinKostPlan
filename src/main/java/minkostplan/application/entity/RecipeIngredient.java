0a3ae5dfa414c5198f3fd2d5796b55d084d9cc23
Emilkr <Emilkroldrup@outlook.com>
2024-05-09 22:35:29 +0200

    feat: Refactor RecipeIngredient class to include constructors, getters, setters, and toString method

--- src/main/java/minkostplan/application/entity/RecipeIngredient.java
+++ src/main/java/minkostplan/application/entity/RecipeIngredient.java
@@ -6,5 +6,47 @@ public class RecipeIngredient {
     private Ingredient ingredient;
     private String quantity;
 
-    // Constructors, Getters, and Setters
+    // Constructors, Getters, and Setters and tostring
+
+    public RecipeIngredient() {
+    }
+
+    public RecipeIngredient(Recipe recipe, Ingredient ingredient, String quantity) {
+        this.recipe = recipe;
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+    }
+
+    public Recipe getRecipe() {
+        return this.recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return this.ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " recipe='" + getRecipe() + "'" +
+            ", ingredient='" + getIngredient() + "'" +
+            ", quantity='" + getQuantity() + "'" +
+            "}";
+    }
 }
