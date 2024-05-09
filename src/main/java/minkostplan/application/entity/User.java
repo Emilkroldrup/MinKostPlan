7b4b2ef7766169abd92ec504b7c2868d5af061db
Emilkr <Emilkroldrup@outlook.com>
2024-05-09 22:38:08 +0200

    Refactor User class to include constructors, getters, setters, and toString method

--- src/main/java/minkostplan/application/entity/User.java
+++ src/main/java/minkostplan/application/entity/User.java
@@ -10,6 +10,68 @@ public class User {
     private String passwordHash;
     private LocalDateTime createdAt;
 
-    // Constructors, Getters, and Setters
+    // Constructors, Getters, and Setters and tostring
+
+    public User() {
+    }
+
+    public User(Long userId, String username, String email, String passwordHash, LocalDateTime createdAt) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.createdAt = createdAt;
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return this.passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " userId='" + getUserId() + "'" +
+            ", username='" + getUsername() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", passwordHash='" + getPasswordHash() + "'" +
+            ", createdAt='" + getCreatedAt() + "'" +
+            "}";
+    }
 }
 
