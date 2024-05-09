0dca198e206e9d26f73695b831cfc02c7f2b3cb9
Emilkr <Emilkroldrup@outlook.com>
2024-05-09 22:37:29 +0200

    feat: Refactor Subscription class to include constructors, getters, setters, and toString method

--- src/main/java/minkostplan/application/entity/Subscription.java
+++ src/main/java/minkostplan/application/entity/Subscription.java
@@ -10,5 +10,65 @@ public class Subscription {
     private LocalDateTime endDate;
     private String status;
 
-    // Constructors, Getters, and Setters
+    public Subscription() {
+    }
+
+    public Subscription(Long subscriptionId, User user, LocalDateTime startDate, LocalDateTime endDate, String status) {
+        this.subscriptionId = subscriptionId;
+        this.user = user;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public Long getSubscriptionId() {
+        return this.subscriptionId;
+    }
+
+    public void setSubscriptionId(Long subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return this.endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " subscriptionId='" + getSubscriptionId() + "'" +
+            ", user='" + getUser() + "'" +
+            ", startDate='" + getStartDate() + "'" +
+            ", endDate='" + getEndDate() + "'" +
+            ", status='" + getStatus() + "'" +
+            "}";
+    }
 }
