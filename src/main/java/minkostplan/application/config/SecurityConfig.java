5047124482e46662f102c630f8615dfcc232277b
Spi0nDK <SpionGamingDK@gmail.com>
2024-05-15 21:57:02 +0200

    Adminpage HTML er lavet

--- src/main/java/minkostplan/application/config/SecurityConfig.java
+++ src/main/java/minkostplan/application/config/SecurityConfig.java
@@ -33,7 +33,6 @@ public class SecurityConfig {
                         new OrRequestMatcher(
                             new AntPathRequestMatcher("/login"),
                             new AntPathRequestMatcher("/home"),
-                            new AntPathRequestMatcher("/admin"),
                             new AntPathRequestMatcher("/UserCreation"),
                             new AntPathRequestMatcher("/**/*.css"),
                             new AntPathRequestMatcher("/**/*.jpg"),
