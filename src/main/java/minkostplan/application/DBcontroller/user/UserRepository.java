e13e4223eca64145eaf7cedf31bbea23afb586d6
Emilkr <EmilKroldrup@outlook.com>
2024-05-17 10:58:38 +0200

    start of merge

--- src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
+++ src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
@@ -2,7 +2,6 @@ package minkostplan.application.DBcontroller.user;
 
 import minkostplan.application.entity.Users;
 import minkostplan.application.DBcontroller.SimpleDataAccess;
-import org.springframework.stereotype.Component;
 
 
 public interface UserRepository extends SimpleDataAccess<Users> {
