c4e2636f81f438b97e349fd2e23b1bc324966c84
Mads505m <73273757+Mads505m@users.noreply.github.com>
2024-05-18 22:30:07 +0200

    editprofiledetails
    Den en er funktionel nu. Frontend skal bare laves om

--- src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
+++ src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Component;
 public interface UserRepository extends SimpleDataAccess<Users> {
     Users findByUsername(String username);
     Users findByEmail(String email);
+    void editUserDetails(Users users);
 }
--- src/main/java/minkostplan/application/DBcontroller/user/UserRepositoryImpl.java
+++ src/main/java/minkostplan/application/DBcontroller/user/UserRepositoryImpl.java
@@ -3,7 +3,9 @@ package minkostplan.application.DBcontroller.user;
 import minkostplan.application.entity.Users;
 import minkostplan.application.DBcontroller.GenericJdbcRepository;
 
+import minkostplan.application.usecase.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +14,11 @@ import java.util.List;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
     private GenericJdbcRepository<Users> dataAccess;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public UserRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
         this.dataAccess = new GenericJdbcRepository<>(jdbcTemplate, Users.class);
     }
 
@@ -36,5 +40,20 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public List<Users> findAll() {
         return dataAccess.findAll();
+    }
+
+    @Override
+    public void editUserDetails(Users users) {
+        Users currentuser = UserUtil.getCurrentUser();
+        try {
+            String sql = "UPDATE users SET username = COALESCE(?, username), email = COALESCE(?, email) WHERE email =?";
+            if( !users.getUsername().isEmpty() && !users.getEmail().isEmpty()){
+                jdbcTemplate.update(sql, users.getUsername(), users.getEmail(), currentuser.getEmail());
+            }
+        } catch (DuplicateKeyException duplicateKeyException){
+            System.out.println("duplicatekey error, same user-details already exists" + duplicateKeyException);
+        } catch (Exception e) {
+            System.out.println("Error trying to update user: " + e.getMessage());
+        }
     }
 }
