0c8858015586a04a6bd4fa2b85211ee6ba05a90f
Emilkr <Emilkroldrup@outlook.com>
2024-05-20 01:17:18 +0200

    chore: Refactor UserRepository and add method documentation

--- src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
+++ src/main/java/minkostplan/application/DBcontroller/user/UserRepository.java
@@ -3,9 +3,31 @@ package minkostplan.application.DBcontroller.user;
 import minkostplan.application.entity.Users;
 import minkostplan.application.DBcontroller.SimpleDataAccess;
 
-
+/**
+ * User repository interface for user-specific data access operations.
+ */
 public interface UserRepository extends SimpleDataAccess<Users> {
+
+    /**
+     * Finds a user by their username.
+     *
+     * @param username the username
+     * @return the found user
+     */
     Users findByUsername(String username);
+
+    /**
+     * Finds a user by their email.
+     *
+     * @param email the email
+     * @return the found user
+     */
     Users findByEmail(String email);
+
+    /**
+     * Edits the details of a user.
+     *
+     * @param users the user entity with updated details
+     */
     void editUserDetails(Users users);
 }
