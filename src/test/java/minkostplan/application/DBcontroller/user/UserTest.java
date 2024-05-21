34528286ab4da6160515ac8b346df6f7d789466a
Mads505m <onlinebigchungus@gmail.com>
2024-05-21 10:22:56 +0200

    unittest update

--- src/test/java/minkostplan/application/DBcontroller/user/UserTest.java
+++ src/test/java/minkostplan/application/DBcontroller/user/UserTest.java
@@ -1,11 +1,16 @@
 package minkostplan.application.DBcontroller.user;
 import minkostplan.application.entity.Users;
+import minkostplan.application.usecase.UserUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 @SpringBootTest
@@ -14,7 +19,9 @@ class UserRepositoryTest {
 
     @Mock
     private UserRepository userRepository;
+    
 
+<<<<<<< Updated upstream
     @Test
     void findByEmail(){
       String email ="test@test.com";
@@ -34,13 +41,41 @@ class UserRepositoryTest {
       String username ="test";
       Users user = new Users("test","test@test.com","testword", LocalDateTime.now());
       when(userRepository.findByUsername(username)).thenReturn(user);
+=======
+    private Users existingUser;
+    private String username;
+>>>>>>> Stashed changes
 
-      Users result = userRepository.findByUsername(username);
+    private String email;
 
-      assertEquals(user,result);
-
-      System.out.println("Has to equal: " + user);
-      System.out.println();
-      System.out.println("Result: " + result);
+    @BeforeEach
+    void setUp() {
+        username = "test";
+        email = "testemail@gmail.com";
+        existingUser = new Users(username, email, "testword", LocalDateTime.now());
     }
+<<<<<<< Updated upstream
 }
+=======
+
+
+
+    @Test
+    void findbyEmail(){
+
+        when(userRepository.findByEmail(email)).thenReturn(existingUser);
+
+        Users result = userRepository.findByEmail(email);
+
+        assertEquals(existingUser,result);
+
+        System.out.println(email);
+
+        System.out.println("Has to equal: " + existingUser);
+        System.out.println();
+        System.out.println("Result: " + result);
+    }
+
+
+}
+>>>>>>> Stashed changes
