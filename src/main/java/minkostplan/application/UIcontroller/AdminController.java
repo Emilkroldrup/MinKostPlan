8cc1d346c000e73d3f7b6e381c376334ca9105a8
Mads505m <onlinebigchungus@gmail.com>
2024-05-23 18:18:20 +0200

    adding Ingridients controller and js logic, and java doc for admincontroller
    Change some html (hidden inputfields) Used it in js and controller to connect inputs to the js and then add the inputted the data to the database, in which the function was already made.

--- src/main/java/minkostplan/application/UIcontroller/AdminController.java
+++ src/main/java/minkostplan/application/UIcontroller/AdminController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 /**
  * Controller for handling admin related requests.
@@ -30,13 +32,13 @@ public class AdminController {
 
     /**
      * Handles the admin page request.
-     *
+     * @param model for recipe, used to pass data from controller to view in our html
      * @return the admin page view
      */
     @GetMapping("/admin")
     public String adminPage(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = auth.getName(); // get logged-in username
+        String email = auth.getName();
 
         if (!email.equals(adminEmail)) {
             return "redirect:/access-denied";
@@ -45,10 +47,31 @@ public class AdminController {
         return "adminpage";
     }
 
+    /**
+     * Adds the recipe and ingredient(s) to the database
+     * @param recipe recipe object of recipe class
+     * @param ingredients ingredients value of hidden input fields
+     * @param descriptions descriptions value of hidden input fields
+     * @return the admin page view
+     */
+
     @PostMapping("/addrecipe")
-    public String addRecipe(@ModelAttribute("recipe") Recipe recipe, @ModelAttribute("ingredients") Ingredient ingredient) {
-        ingredientsRepository.saveIngredient(ingredient);
+    public String addRecipe(@ModelAttribute("recipe") Recipe recipe, @RequestParam("ingredients") List<String> ingredients,
+                            @RequestParam("descriptions") List<String> descriptions) {
+
         recipeRepository.saveRecipe(recipe);
+
+        for (int i = 0; i < ingredients.size(); i++) {
+            String ingredientName = ingredients.get(i);
+            String description = descriptions.get(i);
+
+            Ingredient ingredient = new Ingredient();
+            ingredient.setName(ingredientName);
+            ingredient.setDescription(description);
+
+            ingredientsRepository.saveIngredient(ingredient);
+        }
         return "adminpage";
     }
+
 }
