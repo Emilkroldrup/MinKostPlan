2ff6171b0188627bfb15e1d5ce105e981737755b
Mads505m <onlinebigchungus@gmail.com>
2024-06-02 02:04:27 +0200

    expcetion

--- src/main/java/minkostplan/application/usecase/SurveyService.java
+++ src/main/java/minkostplan/application/usecase/SurveyService.java
@@ -3,6 +3,8 @@ package minkostplan.application.usecase;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,9 +25,14 @@ public class SurveyService{
     }
 
     public Users createUser(String firstName, String lastName, int age, int height, int weight, String gender, String goal, String email, String password, LocalDateTime createdAt, String activityLevel) {
-        String passwordHash = passwordEncoder.encode(password);
-        Users user = new Users(firstName, lastName, age, height, weight, gender, goal, email, passwordHash, LocalDateTime.now(), activityLevel);
-        userRepository.saveUser(user);
-        return user;
+        try{
+            String passwordHash = passwordEncoder.encode(password);
+            Users user = new Users(firstName, lastName, age, height, weight, gender, goal, email, passwordHash, LocalDateTime.now(), activityLevel);
+            userRepository.saveUser(user);
+            return user;
+        } catch (DuplicateKeyException duplicateKeyException){
+            System.out.println("Same user email is already being used" + duplicateKeyException.getMessage());
+        }
+      return null;
     }
 }
