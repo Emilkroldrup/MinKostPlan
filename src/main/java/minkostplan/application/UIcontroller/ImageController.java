dca2d7a824e39885a77d203f5486c214678a909d
minkguel <mikkel.ostvand.pedersen@gmail.com>
2024-06-03 10:02:55 +0200

    Alle generelle image/picture klasse oprettet

---
+++ src/main/java/minkostplan/application/UIcontroller/ImageController.java
@@ -0,0 +1,133 @@
+package minkostplan.application.UIcontroller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import minkostplan.application.DBcontroller.picture.PictureStorage;
+import minkostplan.application.entity.ImageInfo;
+
+@Controller
+public class ImageController {
+
+    @Autowired
+    PictureStorage pictureStorage;
+    
+
+    /**
+     * Redirects to the home page.
+     * @return the home page
+     * 
+     */
+    @GetMapping("/")
+    public String homepage() {
+        return "redirect:/home";
+    }
+
+    /**
+     * Redirects to the home page.
+     * @return the upload form
+     * 
+     */
+    @GetMapping("/images/new")
+    public String newImage (Model model)  {
+        return "upload_form";
+    }
+
+   
+    /**
+     * Uploads the specified image.
+     * @param model
+     * @param file
+     * @return the upload form
+     */
+    @PostMapping("/images/upload")
+    public String uploadImage(Model model, @RequestParam("file") MultipartFile file) {
+        String message = "";
+
+        try {
+            pictureStorage.save(file);
+
+            message = "Uploaded the image succesfully: " + file.getOriginalFilename();
+            model.addAttribute("message", message);
+        } catch (Exception e) {
+            message = "Could not upload the image: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
+            model.addAttribute("message", message);
+        }
+
+        return "upload_form";
+    }
+
+
+    /**
+     * Lists all the images in the uploads folder.
+     * @param model
+     * @return the images view
+     */
+    //  Might be useful for the recipes
+    @GetMapping("/images")
+    public String getListImages(Model model) {
+        List<ImageInfo> imageInfos = pictureStorage.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder.fromMethodName(ImageController.class, "getImage", path.getFileName().toString()).build().toString();
+
+            return new ImageInfo(filename, url);
+        }).collect(Collectors.toList());
+
+        model.addAttribute("images", imageInfos);
+        return "images";
+    }
+
+
+    /**
+     * Gets the specified image.
+     * @param filename
+     * @return
+     */
+    // Use this to load the users profile picture when they login
+    @GetMapping("/images/{filename:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        Resource file = pictureStorage.load(filename);
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        
+    }
+
+
+    /**
+     * Deletes the specified image.
+     * @param filename
+     * @param model
+     * @param redirectAttributes
+     * @return the images view
+     */
+    public String deleteImage(@PathVariable String filename, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            boolean existed = pictureStorage.delete(filename);
+
+            if (existed) {
+                redirectAttributes.addFlashAttribute("message", "Delete the image succesfully: " + filename);
+            } else {
+                redirectAttributes.addFlashAttribute("message", "The image does not exist.");
+            }
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "Could not delete the image: " + filename + ". Error: " + e.getMessage());
+        }
+
+        return "redirect:/images";
+
+    }
+}
---
+++ src/main/java/minkostplan/application/usecase/PictureStorageService.java
@@ -0,0 +1,130 @@
+package minkostplan.application.usecase;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import minkostplan.application.DBcontroller.picture.PictureStorage;
+
+
+
+@Service
+public class PictureStorageService implements PictureStorage {
+    private final Path root = Paths.get("./uploads");
+
+    /**
+     * Initializes the folder for the upload.
+     *  If the folder doesn't exist, it creates it.
+     * If the folder already exists, it does nothing.
+     * If the folder can't be created, it throws a runtime exception.
+     * 
+     */
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't initialize folder for upload.");
+        }
+    }
+
+    /**
+     * Saves the specified file in the root folder.
+     * If the file already exists, it throws a runtime exception.
+     *  If the file can't be saved, it throws a runtime exception.
+     *  @param file the file to save
+     *  @throws RuntimeException if the file already exists or can't be saved
+     * 
+     */
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file with that name already exists.");
+            }
+        }
+    }
+
+    /**
+     * Loads the specified file from the root folder.
+     * If the file can't be read, it throws a runtime exception.
+     * @param filename the name of the file to load
+     * @return the resource of the file
+     * @throws RuntimeException if the file can't be read
+     * 
+     */
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file.");
+            }
+        } catch (MalformedURLException e) { 
+            throw new RuntimeException("Error: " + e.getMessage()); 
+        }
+    }
+    
+    /**
+     * Deletes all files in the root folder.
+     * If the files can't be deleted, it throws a runtime exception.
+     *  @throws RuntimeException if the files can't be deleted
+     * 
+     */
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    /**
+     * Loads all files from the root folder.
+     * If the files can't be loaded, it throws a runtime exception.
+     * @return the stream of paths of the files
+     * @throws RuntimeException if the files can't be loaded
+     * 
+     */
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the files.");
+        }
+    }
+
+    /**
+     * Deletes the specified file from the root folder.
+     * If the file can't be deleted, it throws a runtime exception.
+     * @param filename the name of the file to delete
+     * @return true if the file was deleted, false otherwise
+     * @throws RuntimeException if the file can't be deleted
+     *  
+     */
+    @Override
+    public boolean delete(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            return Files.deleteIfExists(file);
+          } catch (IOException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+          }
+    }
+
+}
+
