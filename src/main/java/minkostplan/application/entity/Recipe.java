866666365e3e6ce04453243daf5a436a91b57fda
Emilkr <Emilkroldrup@outlook.com>
2024-06-03 12:12:55 +0200

    chore: Update Recipe entity constructor and add mealType field

--- src/main/java/minkostplan/application/entity/Recipe.java
+++ src/main/java/minkostplan/application/entity/Recipe.java
@@ -18,11 +18,21 @@ public class Recipe {
     private String mealType;
     private List<String> instructionsList;
 
-    // Default constructor
+    /**
+     * Default constructor.
+     */
     public Recipe() {
     }
 
-    // Constructor with all fields
+    /**
+     * Constructs a new Recipe with specified details.
+     *
+     * @param name the name of the recipe
+     * @param cookName the name of the cook
+     * @param averageTime the average time to prepare the recipe
+     * @param createdAt the creation date and time of the recipe
+     * @param instructions the list of instructions for the recipe
+     */
     public Recipe(String name, String cookName, Integer averageTime, LocalDateTime createdAt, String instructions, String mealType) {
         this.name = name;
         this.cookName = cookName;
