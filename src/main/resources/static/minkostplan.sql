bc5cc0ff85f42984e778c0e82120e7acb2ac29a3
Mads505m <onlinebigchungus@gmail.com>
2024-06-06 15:45:57 +0200

    sql

--- src/main/resources/static/minkostplan.sql
+++ src/main/resources/static/minkostplan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: 127.0.0.1:3306
--- Genereringstid: 30. 05 2024 kl. 08:24:22
+-- Genereringstid: 06. 06 2024 kl. 13:45:41
 -- Serverversion: 8.2.0
 -- PHP-version: 8.2.13
 
@@ -32,23 +32,22 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
                                              `ingredient_id` int NOT NULL AUTO_INCREMENT,
                                              `name` varchar(255) NOT NULL,
                                              `description` text,
-                                             `Calories` int DEFAULT NULL,
-                                             `Carbohydrate` int DEFAULT NULL,
-                                             `Fat` int DEFAULT NULL,
+                                             `calories` int DEFAULT NULL,
+                                             `carbohydrate` int DEFAULT NULL,
+                                             `fat` int DEFAULT NULL,
                                              `protein` int DEFAULT NULL,
                                              PRIMARY KEY (`ingredient_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Contains different ingredients used in recipes.';
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Contains different ingredients used in recipes.';
 
 --
 -- Data dump for tabellen `ingredients`
 --
 
-INSERT INTO `ingredients` (`ingredient_id`, `name`, `description`, `Calories`, `Carbohydrate`, `Fat`, `protein`) VALUES
-                                                                                                                     (1, 'Spaghetti', 'Long, thin, cylindrical pasta of Italian origin', 0, 0, 0, 0),
-                                                                                                                     (2, 'Egg', 'Organic free-range eggs', 0, 0, 0, 0),
+INSERT INTO `ingredients` (`ingredient_id`, `name`, `description`, `calories`, `carbohydrate`, `fat`, `protein`) VALUES
+                                                                                                                     (1, 'Spaghettii', 'asfsd', 2, 3, 4, 5),
+                                                                                                                     (2, 'Egg', 'Organic free-range eggs', 2, 0, 0, 0),
                                                                                                                      (3, 'Bacon', 'Thick slices of smoked bacon', 0, 0, 0, 0),
-                                                                                                                     (4, 'Zucchini', 'Fresh zucchini', 0, 0, 0, 0),
-                                                                                                                     (5, 'Tomato', 'Ripe tomatoes', 0, 0, 0, 0);
+                                                                                                                     (4, 'Zucchini', 'Fresh zucchini', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -87,17 +86,19 @@ CREATE TABLE IF NOT EXISTS `recipes` (
                                          `average_time` int NOT NULL COMMENT 'in minutes',
                                          `created_at` datetime NOT NULL,
                                          `instructions` text,
+                                         `meal_type` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
                                          PRIMARY KEY (`recipe_id`),
                                          UNIQUE KEY `unique_recipe_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Stores details about various recipes.';
+) ENGINE=InnoDB AUTO_INCREMENT=173 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Stores details about various recipes.';
 
 --
 -- Data dump for tabellen `recipes`
 --
 
-INSERT INTO `recipes` (`recipe_id`, `name`, `cook_name`, `average_time`, `created_at`, `instructions`) VALUES
-                                                                                                           (1, 'Spaghetti Carbonara', 'Chef Gino', 30, '2024-05-09 09:57:03', NULL),
-                                                                                                           (2, 'Ratatouille', 'Chef Rémy', 45, '2024-05-09 09:57:03', NULL);
+INSERT INTO `recipes` (`recipe_id`, `name`, `cook_name`, `average_time`, `created_at`, `instructions`, `meal_type`) VALUES
+                                                                                                                        (127, 'kyyling', 'mads', 4, '2024-06-04 23:25:49', 's', 'asd'),
+                                                                                                                        (171, 'Pølser', 'Peter', 30, '2024-06-06 13:41:04', 'Put pølser i en gryde', ''),
+                                                                                                                        (172, 'sf', 'sdf', 3, '2024-06-06 14:32:39', 'sdf', NULL);
 
 -- --------------------------------------------------------
 
@@ -119,10 +120,10 @@ CREATE TABLE IF NOT EXISTS `recipe_ingredients` (
 --
 
 INSERT INTO `recipe_ingredients` (`recipe_id`, `ingredient_id`, `quantity`) VALUES
-                                                                                (1, 1, '200 grams'),
-                                                                                (1, 2, '4 units'),
-                                                                                (1, 3, '100 grams'),
-                                                                                (2, 4, '150 grams');
+                                                                                (127, 1, 'sfds'),
+                                                                                (127, 1, 'sfds'),
+                                                                                (171, 1, '200gram'),
+                                                                                (172, 1, '2');
 
 -- --------------------------------------------------------
 
@@ -171,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `users` (
                                        `created_at` datetime NOT NULL,
                                        PRIMARY KEY (`user_id`),
                                        UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Stores information about the users.';
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Stores information about the users.';
 
 --
 -- Data dump for tabellen `users`
@@ -180,7 +181,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `age`, `height`, `weight`, `gender`, `activityLevel`, `goal`, `email`, `password_hash`, `created_at`) VALUES
                                                                                                                                                                    (1, 'John', 'Doe', 30, 180, 80, 'male', '1-2 times a week', 'Build muscle', 'john.doe@example.com', '$2a$12$zm4obx9Li5PBVNq/VOC73e6/8ACR7tPBLrRdqZF8x622EKtilRutW', '2024-05-09 09:56:28'),
                                                                                                                                                                    (2, 'Jane', 'Smith', 22, 165, 55, 'female', '3-5 times a week', 'Gain weight', 'jane.smith@example.com', 'hashed_password2', '2024-05-09 09:56:28'),
-                                                                                                                                                                   (3, 'test', 'testen', 23, 1734, 72, 'Male', '1-2 times a day', 'Lose weight', 'test@mail.com', '$2a$12$nqslPLAkzrttWqMWsTsTfOBSJrhz9QKn7joYOKp2Z6f8DaaOsqQpK', '2024-05-28 14:26:32');
+                                                                                                                                                                   (3, 'test', 'testen', 22, 1734, 71, 'Male', '1-2 gange om ugen', 'Lose weight', 'test@mail.com', '$2a$12$nqslPLAkzrttWqMWsTsTfOBSJrhz9QKn7joYOKp2Z6f8DaaOsqQpK', '2024-05-28 14:26:32'),
+                                                                                                                                                                   (18, 'test', 'test', 3, 3, 3, 'Male', '3-5 times a week', 'Maintain weight', 'onlinebigchungus@gmail.com', '$2a$10$hboOLU2pTntgvJW2.QfpLOnkufMhaOETI0XLEPgiFkDsd5NBy0jA.', '2024-05-30 11:08:10'),
+                                                                                                                                                                   (20, 'Mads', 'Madsen', 2, 3, 2, 'Male', '1-2 times a day', 'Maintain weight', 'onlineasdgus@gmail.com', '$2a$10$GGGZz0DALKv9Mq/j7wJn9.gMx6xvZtQQOCjFh0SfqefaRbQBm7d/m', '2024-06-01 23:44:42'),
+                                                                                                                                                                   (21, 'asda', 'dasdad', 2, 2, 2, 'Male', '1-2 times a day', 'Maintain weight', 'asdsda@adsad.com', '$2a$10$Ugs5WzxFsL/2sdFpiOZIUukgLiG3N20AXDzmITfB5CdeDBYVjIpIS', '2024-06-03 01:18:51');
 
 --
 -- Begrænsninger for dumpede tabeller
@@ -196,6 +200,8 @@ ALTER TABLE `profilepictures`
 -- Begrænsninger for tabel `recipe_ingredients`
 --
 ALTER TABLE `recipe_ingredients`
+    ADD CONSTRAINT `fk_ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `fk_recipe_id` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`) ON DELETE CASCADE,
     ADD CONSTRAINT `ingredients_fk` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`),
     ADD CONSTRAINT `recipe_fk` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`);
 
